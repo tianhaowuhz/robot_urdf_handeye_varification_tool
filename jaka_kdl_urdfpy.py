86db70ed01d185647bc6de35cb16ceb9b88f3208
thwu <11071192+thwu17@user.noreply.gitee.com>
2024-11-29 09:47:34 +0800

    update vis

--- jaka_kdl_urdfpy.py
+++ jaka_kdl_urdfpy.py
@@ -2,66 +2,6 @@ from urdfpy import URDF
 from math import pi as PI
 import os
 
-robot = URDF.load('assets/jakamini_leaphand.urdf')
-
-for link in robot.links:
-    print(link.name)
-
-config = {}
-for joint in robot.actuated_joints:
-    print(joint.name)
-    config[joint.name] = 0.0
-
-config['joint_1'] = -PI/2
-config['joint_2'] = -PI*2.2/180
-config['joint_3'] = -PI*90/180
-config['joint_4'] = 0
-config['joint_5'] = -PI*90/180
-config['joint_6'] = -PI*90.5/180
-
-robot.show(config)
-
-
-import torch
-traj = torch.load("demonstration_1.pth")
-
-for idx, joint_traj in enumerate(traj['arm_abs_joint'][0]):
-    joint_name = f"joint_{idx+1}"
-    # copy last element of joint_traj for 10 steps
-    # joint_traj = torch.cat([joint_traj, joint_traj[-1].repeat(10)])
-    config[joint_name] = joint_traj.cpu().numpy()
-fk = robot.link_fk(config)
-
-arm_joint_traj = traj['arm_abs_joint'].permute(1,0)
-hand_joint_traj = traj['hand_abs_joint'].permute(1,0)
-
-for idx, joint_traj in enumerate(arm_joint_traj):
-    joint_name = f"joint_{idx+1}"
-    # copy last element of joint_traj for 10 steps
-    # joint_traj = torch.cat([joint_traj, joint_traj[-1].repeat(10)])
-    config[joint_name] = joint_traj.cpu().numpy()
-
-for idx, joint_traj in enumerate(hand_joint_traj):
-    if idx == 0:
-        joint_name = "1"
-    elif idx == 1:
-        joint_name = "0"
-    elif idx == 4:
-        joint_name = "5"
-    elif idx == 5:
-        joint_name = "4"
-    elif idx == 8:
-        joint_name = "9"
-    elif idx == 9:
-        joint_name = "8"
-    else:
-        joint_name = f"{idx}"
-    # joint_traj = torch.cat([joint_traj, joint_traj[-1].repeat(10)])
-    config[joint_name] = joint_traj.cpu().numpy()
-
-robot.animate(cfg_trajectory=config, loop_time=13)
-print(traj.shape)
-
 class RobotKDL(object):
     def __init__(self,
                  urdf_path: str = "jakamini_leaphand.urdf",
--- vis_urdf_via_urdfpy.py
+++ vis_urdf_via_urdfpy.py
@@ -1,6 +1,32 @@
 from urdfpy import URDF
 from math import pi as PI
 from termcolor import cprint
+import numpy as np
+
+def get_link_names(robot):
+    link_names = []
+    for link in robot.links:
+        link_names.append(link.name)
+    return link_names
+
+def get_joint_names(robot):
+    joint_names = []
+    for joint in robot.joints:
+        joint_names.append(joint.name)
+    return joint_names
+
+def get_actuated_joint_names(robot):
+    joint_names = []
+    for joint in robot.actuated_joints:
+        joint_names.append(joint.name)
+    return joint_names
+
+def vis_robot_trajectory(robot, actuated_joint_names, traj):
+    for idx, joint_traj in enumerate(traj):
+        config = {}
+        for idx, actuated_joint_name in enumerate(actuated_joint_names):
+            config[actuated_joint_name] = joint_traj[idx]
+        robot.animate(cfg_trajectory=config, loop_time=13)
 
 if __name__ == "__main__":
     urdf_path = "assets/jakamini_leaphand.urdf"
@@ -8,26 +34,29 @@ if __name__ == "__main__":
     robot = URDF.load(urdf_path)
     
     cprint("Robot Links:", "green")
-    link_names = []
-    for link in robot.links:
-        link_names.append(link.name)
+    link_names = get_link_names(robot)
     print(link_names)
-
-    cprint("Robot Joints:", "green")
-    config = {}
-    joint_names = []
-    for joint in robot.actuated_joints:
-        joint_names.append(joint.name)
-        config[joint.name] = 0.0
-    print(joint_names)
     
-    # custom joint value here
-    config['joint_1'] = -PI/2
-    config['joint_2'] = -PI*2.2/180
-    config['joint_3'] = -PI*90/180
-    config['joint_4'] = 0
-    config['joint_5'] = -PI*90/180
-    config['joint_6'] = -PI*90.5/180
+    cprint("Robot Joints:", "green")
+    joint_names = get_joint_names(robot)
+    print(joint_names)
 
-    cprint("Robot Show:", "green")
-    robot.show(config)
+    cprint("Robot Actuated Joints:", "green")
+    actuated_joint_names = get_actuated_joint_names(robot)
+    print(actuated_joint_names)
+    
+    cprint("Show Robot with Zero Joint Configuration", "green")
+    zero_joint_config = {}
+    for actuated_joint_name in actuated_joint_names:
+        zero_joint_config[actuated_joint_name] = 0
+    robot.show(zero_joint_config)
+
+    cprint("Show Robot Traj", "green")
+    example_joint_traj = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
+    joint_traj_config = {}
+    for actuated_joint_name in actuated_joint_names:
+        joint_traj_config[actuated_joint_name] = example_joint_traj
+    robot.animate(cfg_trajectory=joint_traj_config, loop_time=5)
+
+    
+    
